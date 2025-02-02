64ff103b4558ac50f6603a9977d5220dc49a48fb
bence-balazs <bestbence@gmail.com>
2025-02-02 18:21:59 +0100

    fix: vscode

--- ubuntuSway.sh
+++ ubuntuSway.sh
@@ -66,12 +66,15 @@ install_firefox() {
 }
 
 # setup vscode repository and install it
-setup_vscodium() {
+setup_vscode() {
     apt-get install wget gpg
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
     rm -f packages.microsoft.gpg
+    apt install apt-transport-https
+    apt update
+    apt install code # or code-insiders
 }
 
 # setup terraform
@@ -183,7 +186,7 @@ case "$1" in
         # install_packages_for_sway
         # install_packages_for_gnome
         install_firefox
-        setup_code
+        setup_vscode
         setup_terraform
         setup_docker
         install_k9s
